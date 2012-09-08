c21abd3db4d6b6ce9209a54fb894174abc4e1f8b
Tarzan <hocdt85@gmail.com>
2012-09-08 11:50:03 +0700

    finish layout

---
+++ public_html/index.php
@@ -0,0 +1,18 @@
+<?php
+
+define('ROOT_PATH', realpath(__DIR__.'/..'));
+define('HOME_PATH', ROOT_PATH.'/public_html');
+define('CODE_PATH', ROOT_PATH.'/protected');
+define('HOME_URL', '');
+
+// change the following paths if necessary
+$yii=ROOT_PATH.'/yii/framework/yii.php';
+$config=CODE_PATH.'/config/main.php';
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
