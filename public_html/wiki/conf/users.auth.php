58f7c6600f4c8096ffc7b5e6bac4209d872ba879
Tarzan <hocdt85@gmail.com>
2012-09-24 16:36:33 +0700

    dokuwiki

---
+++ public_html/wiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+tarzan:$1$kFp3XHfc$R8hE.7d73F5mb/dhnfsRR0:Học Đỗ:hocdt85@gmail.com:admin,user
